05b04e81a3fe1eb8acec0ae1d230bd3139ac5326
Kshitiz Sharan <500070933@stu.upes.ac.in>
2022-04-24 14:52:31 +0530

    pipeline

--- blog.py
+++ blog.py
@@ -1,8 +1,53 @@
 import os
 import openai
+import re
+
 key1= 'sk-jziF2dNB5GdlLPtT9X9dT3'
 key2='BlbkFJHgRWMdMb4rv6D4GuWj6z'
 openai.api_key = f"{key1}{key2}"
+
+
+
+
+
+def generateBlogTopicsPipe(prompt1):
+    article=[]
+    response = openai.Completion.create(
+      engine="davinci-instruct-beta-v3",
+      prompt="Generate blog topics on: {}. \n \n 1.  ".format(prompt1),
+      temperature=0.7,
+      max_tokens=100,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
+    )
+    temp=response['choices'][0]['text'].split('?')
+    length=len(temp)
+    #for i in range(0,length):
+    temp1=generateBlogTopics(temp[0])
+    temp1=temp1.replace('\n','').replace('.','').replace('1','').replace('2','').replace('3','').replace('4','').replace('5','').replace('6','').replace('7','').replace('8','').replace('9','')
+    temp1=temp1.split('?')
+
+
+    temp2=generateBlogSections(temp1[0])
+    #article.append(temp2)
+    temp2=temp2.replace('-','')
+    temp2=temp2.split(':')
+
+    length=len(temp2)
+    for i in range(0,length):
+      temp3=blogSectionExpander(temp2[i])
+      article.append(temp3)
+      finalarticle=' '.join(article)
+
+
+    #print(temp)
+    #return response['choices'][0]['text']
+    return finalarticle
+
+
+
+
 
 def generateBlogTopics(prompt1):
     response = openai.Completion.create(
@@ -36,10 +81,12 @@ def blogSectionExpander(prompt1):
       engine="davinci-instruct-beta-v3",
       prompt="Expand the blog section in to a detailed professional , witty and clever explanation.\n\n {}".format(prompt1),
       temperature=0.7,
-      max_tokens=200,
+      max_tokens=1000,
       top_p=1,
       frequency_penalty=0,
       presence_penalty=0
     )
 
     return response['choices'][0]['text']
+
+#print(generateBlogTopicsPipe('Machine Learning')) 
